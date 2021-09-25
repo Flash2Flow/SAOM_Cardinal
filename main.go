fed8f6ef4be722a67d75844a24e0d0162d5fa661
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-09-26 04:47:47 +1000

    fix link

--- main.go
+++ main.go
@@ -24,32 +24,29 @@ func api(w http.ResponseWriter, r *http.Request) {
 	email := string(email_byte[:])
 	access_drop := string(access_drop_byte[:])
 	token := string(token_byte[:])
-	log.Print(email, access_drop, token)
 	if token == "ac01a9a846016b13e1249040c3bb1c3e" {
-		send("Перейдите по ссылке что бы сбросить пароль - http//site.com/drop.php?0c83f57c786a0b4a39efab23731c7ebc=%s&c1a8a39a96d32cac85fd7bca0d50830b=%s", email, access_drop)
+
+		from := "tmushkaterova@gmail.com"
+		pass := "537003Sa"
+		to := email
+
+		msg := "From: " + from + "\n" +
+			"To: " + to + "\n" +
+			"Subject: SAOM ONLINE Drop Password\n\n" +
+			"Перейдите по ссылке что бы сбросить пароль - <a href='http://cs85079.tmweb.ru/drop.php?0c83f57c786a0b4a39efab23731c7ebc=" + email + "&c1a8a39a96d32cac85fd7bca0d50830b=" + access_drop + "'></a>"
+
+		err := smtp.SendMail("smtp.gmail.com:587",
+			smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
+			from, []string{to}, []byte(msg))
+
+		if err != nil {
+			log.Println("StatusRequest: 401")
+			fmt.Fprint(w, "StatusRequest: 401")
+			log.Printf("smtp error: %s", err)
+			return
+		}
+		log.Println("Sent to, ", to)
 		fmt.Fprint(w, "StatusRequest: 201")
 	}
 
-}
-func send(body string, email string, access_drop string) {
-	from := "tmushkaterova@gmail.com"
-	pass := "537003Sa"
-	to := email
-
-	msg := "From: " + from + "\n" +
-		"To: " + to + "\n" +
-		"Subject: SAOM ONLINE Drop Password\n\n" +
-		body
-
-	err := smtp.SendMail("smtp.gmail.com:587",
-		smtp.PlainAuth("", from, pass, "smtp.gmail.com"),
-		from, []string{to}, []byte(msg))
-
-	if err != nil {
-		log.Println("StatusRequest: 401")
-		log.Printf("smtp error: %s", err)
-		return
-	}
-	log.Println("StatusRequest: 201")
-	log.Println("Sent to, ", to)
 }
