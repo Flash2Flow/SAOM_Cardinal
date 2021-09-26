3ede0471a1bfd05859331536c32f0db556cd998a
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-09-26 20:03:41 +1000

    sadasdsa

--- main.go
+++ main.go
@@ -1,28 +1,15 @@
 package main
 
 import (
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	http.HandleFunc("/api", api)
-	http.ListenAndServe(":"+port, nil)
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":3000", nil)
 }
 
-func api(w http.ResponseWriter, r *http.Request) {
-	log.Print("new request")
-	query := r.URL.Query()
-
-	token_byte := query.Get("3c6e0b8a9c15224a8228b9a98ca1531d")
-
-	token := string(token_byte[:])
-	if token == "ac01a9a846016b13e1249040c3bb1c3e" {
-
-		w.Header().Set("Server", "A Go Web Server")
-		w.WriteHeader(200)
-	}
-
+func foo(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Status code", "200")
+	w.WriteHeader(200)
 }
