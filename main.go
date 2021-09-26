531410d31ede65e55e432df184167e7e915828a9
re-incarnation <re-incarnation@bk.ru>
2021-09-26 20:53:59 +1000

    fix bags res

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"gopkg.in/gomail.v2"
 )
 
 type Cardinal struct {
@@ -22,10 +24,26 @@ func api(w http.ResponseWriter, r *http.Request) {
 	log.Print("new request")
 	query := r.URL.Query()
 
-	token_byte := query.Get("3c6e0b8a9c15224a8228b9a98ca1531d")
+	token_byte := query.Get("token")
+	email_byte := query.Get("email")
+	access_drop_byte := query.Get("access_drop")
 
 	token := string(token_byte[:])
+	email := string(email_byte[:])
+	access_drop := string(access_drop_byte[:])
 	if token == "ac01a9a846016b13e1249040c3bb1c3e" {
+
+		m := gomail.NewMessage()
+		m.SetHeader("From", "tmushkaterova@gmail.com")
+		m.SetHeader("To", email)
+		m.SetHeader("Subject", "SAOM ONLINE Drop Password!")
+		m.SetBody("text/html", "Перейдите по ссылке что бы сбросить пароль - <a href='http://ch37276.tmweb.ru/drop.php?email="+email+"&access_drop="+access_drop+">Link</a>")
+
+		d := gomail.NewPlainDialer("smtp.gmail.com", 587, "tmushkaterova@gmail.com", "537003DOsaV")
+		if err := d.DialAndSend(m); err != nil {
+			panic(err)
+		}
+
 		mss := Cardinal{"Success", []string{"201"}}
 
 		js, err := json.Marshal(mss)
