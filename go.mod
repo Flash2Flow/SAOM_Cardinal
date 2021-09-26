c5bf654c9ca281f52da50b7b9c3cc003df573656
re-incarnation <re-incarnation@bk.ru>
2021-09-27 03:04:43 +1000

    push beta3

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.17
 
 require gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 
-require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc 
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"gopkg.in/gomail.v2"
 )
 
 type Cardinal struct {
 	Status string
 	Code   []string
+}
+
+type Drop struct {
+	Access_drop string
 }
 
 func main() {
@@ -26,46 +32,70 @@ func api(w http.ResponseWriter, r *http.Request) {
 
 	token_byte := query.Get("token")
 	email_byte := query.Get("email")
-	access_drop_byte := query.Get("access_drop")
 
 	token := string(token_byte[:])
 	email := string(email_byte[:])
-	access_drop := string(access_drop_byte[:])
 	if token == "ac01a9a846016b13e1249040c3bb1c3e" {
 
-		m := gomail.NewMessage()
-		m.SetHeader("From", "tmushkaterova@gmail.com")
-		m.SetHeader("To", email)
-		m.SetHeader("Subject", "SAOM ONLINE Drop Password!")
-		m.SetBody("text/html", "Перейдите по ссылке что бы сбросить пароль - <a href='http://ch37276.tmweb.ru/drop.php?email="+email+"&access_drop="+access_drop+"'>Link</a>")
+		db, err := sql.Open("mysql", "re-incarnation:537003ReIncar$Mailen@tcp(37.146.123.137:3306)/poplavok")
 
-		d := gomail.NewPlainDialer("smtp.gmail.com", 587, "tmushkaterova@gmail.com", "537003DOsaV")
-		if err := d.DialAndSend(m); err != nil {
+		if err != nil {
 			log.Print(err)
-			mss := Cardinal{"Internal Server Error", []string{"500"}}
+		}
+
+		defer db.Close()
+
+		res, err := db.Query("SELECT `access_drop` FROM `users` WHERE `email`=?", email)
+		if err != nil {
+			log.Print(err)
+		}
+
+		drop_full := []Drop{}
+		for res.Next() {
+			var drop Drop
+			err = res.Scan(&drop.Access_drop)
+			if err != nil {
+				log.Print(err)
+			}
+			drop_full = append(drop_full, drop)
+		}
+
+		for _, drop := range drop_full {
+			var Body_url = "Перейдите по ссылке что бы сбросить пароль - <a href='http://ch37276.tmweb.ru/drop.php?email=" + email + "&access_drop=" + drop.Access_drop + "'>Link</a>"
+			m := gomail.NewMessage()
+			m.SetHeader("From", "tmushkaterova@gmail.com")
+			m.SetHeader("To", email)
+			m.SetHeader("Subject", "SAOM ONLINE Drop Password!")
+			m.SetBody("text/html", Body_url)
+
+			d := gomail.NewPlainDialer("smtp.gmail.com", 587, "tmushkaterova@gmail.com", "537003DOsaV")
+			if err := d.DialAndSend(m); err != nil {
+				log.Print(err)
+				mss := Cardinal{"Internal Server Error", []string{"500"}}
+				js, err := json.Marshal(mss)
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.Header().Set("Content-Type", "application/json")
+				w.Header().Set("MSS", "Cardinal")
+				w.Write(js)
+				log.Print("Internal Server Error: 500")
+			}
+
+			mss := Cardinal{"Success", []string{"201"}}
+
 			js, err := json.Marshal(mss)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
+
 			w.Header().Set("Content-Type", "application/json")
 			w.Header().Set("MSS", "Cardinal")
 			w.Write(js)
-			log.Print("Internal Server Error: 500")
+			log.Print("Success: 201")
 		}
-
-		mss := Cardinal{"Success", []string{"201"}}
-
-		js, err := json.Marshal(mss)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Header().Set("MSS", "Cardinal")
-		w.Write(js)
-		log.Print("Success: 201")
 	} else {
 		mss := Cardinal{"ErrAuth", []string{"401"}}
 		js, err := json.Marshal(mss)
@@ -78,5 +108,4 @@ func api(w http.ResponseWriter, r *http.Request) {
 		w.Write(js)
 		log.Print("ErrAuth: 401")
 	}
-
 }
