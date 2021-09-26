ebac517e3d7ccd11c51bcef0cca9c38015d52850
re-incarnation <74276034+re-incarnation@users.noreply.github.com>
2021-09-26 20:30:35 +1000

    test

--- main.go
+++ main.go
@@ -1,32 +1,52 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
-		"os"	
-		"encoding/json"
+	"os"
 )
 
-type Profile struct {
-	Name		string
-	Hobbies []string
+type Cardinal struct {
+	Status string
+	Code   []string
 }
 
 func main() {
-		port := os.Getenv("PORT")
-	http.HandleFunc("/", foo)
+	port := os.Getenv("PORT")
+	http.HandleFunc("/api", api)
 	http.ListenAndServe(":"+port, nil)
 }
 
-func foo(w http.ResponseWriter, r *http.Request) {
+func api(w http.ResponseWriter, r *http.Request) {
+	log.Print("new request")
+	query := r.URL.Query()
 
-	profile := Profile{"Alex", []string{"snowboarding", "programming"}}
+	token_byte := query.Get("3c6e0b8a9c15224a8228b9a98ca1531d")
 
-	js, err := json.Marshal(profile)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	token := string(token_byte[:])
+	if token == "ac01a9a846016b13e1249040c3bb1c3e" {
+		mss := Cardinal{"Success", []string{"201"}}
+
+		js, err := json.Marshal(mss)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		log.Print("Success: 201")
+	} else {
+		mss := Cardinal{"ErrAuth", []string{"401"}}
+		js, err := json.Marshal(mss)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+		log.Print("ErrAuth: 401")
 	}
 
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
 }
